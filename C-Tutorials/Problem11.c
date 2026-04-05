352cd8218bcd36dd687c052f2eae30ee1ed807d0
Vishruthiiii <vishruthikaa@gmail.com>
2026-04-05 06:27:54 +0000

    array reversal

---
+++ C-Tutorials/Problem11.c
@@ -0,0 +1,27 @@
+/*The first line contains an integer,n , denoting the size of the array. The next line contains n  space-separated integers denoting the elements of the array*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    int temp;
+    for(int i=0;i<num/2;i++)
+    {
+        temp=arr[i];
+        arr[i]=arr[num-i-1];
+        arr[num-1-i]=temp;
+    }
+
+    /* Write the logic to reverse the array. */
+
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    return 0;
+}
+
