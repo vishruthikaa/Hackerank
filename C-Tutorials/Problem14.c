faeb88f49ea8e83652de84f038945836c61bd354
Vishruthiiii <vishruthikaa@gmail.com>
2026-04-05 06:35:49 +0000

    Calculate the nth term

---
+++ C-Tutorials/Problem14.c
@@ -0,0 +1,42 @@
+/*There is a series,s , where the next term is the sum of pervious three terms. Given the first three terms of the series,a ,b , and c  respectively, you have to output the nth term of the series using recursion.*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+int find_nth_term(int n, int a, int b, int c)
+{
+    if(n==1) return a;
+    if(n==2) return b;
+    if(n==3) return c;
+    /*if(n==6)
+    {
+        return find_nth_term(n-1, a,b,c)+find_nth_term(n-2, a,b,c)+c;
+    }
+    if(n==5)
+    {
+        return find_nth_term(n-1, a,b,c)+b+c;
+    }
+    if(n==4)
+    {
+        return a+b+c;
+    }*/
+    else {
+    {
+        return find_nth_term(n-1, a,b,c)+find_nth_term(n-2,a,b,c)+find_nth_term(n-3,a, b,c);
+    }
+    }
+
+}
+
+int main() {
+    int n, a, b, c;
+
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+
+    printf("%d", ans);
+    return 0;
+}
+
