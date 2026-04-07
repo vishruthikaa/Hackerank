f02433c6d51171dfab243127e4321825fd9d376b
Vishruthiiii <vishruthikaa@gmail.com>
2026-04-07 07:17:03 +0000

    Dynamic arrays

---
+++ C-Tutorials/Problem16.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * This stores the total number of books in each shelf.
+ */
+int* total_number_of_books;
+
+/*
+ * This stores the total number of pages in each book of each shelf.
+ * The rows represent the shelves and the columns represent the books.
+ */
+int** total_number_of_pages;
+
+int main()
+{
+    int total_number_of_shelves;
+    scanf("%d", &total_number_of_shelves);
+    
+    int total_number_of_queries;
+    scanf("%d", &total_number_of_queries);
+    total_number_of_books = (int*)calloc(total_number_of_shelves, sizeof(int));
+total_number_of_pages = (int**)malloc(total_number_of_shelves * sizeof(int*));
+
+for (int i = 0; i < total_number_of_shelves; i++) {
+    *(total_number_of_pages + i) = NULL;
+}
+    
+    while (total_number_of_queries--) {
+        int type_of_query;
+        scanf("%d", &type_of_query);
+        
+        if (type_of_query == 1) 
+        {
+            int x, y;
+    scanf("%d %d", &x, &y);
+
+    int current_books = *(total_number_of_books + x);
+
+    // Increase book count
+    *(total_number_of_books + x) = current_books + 1;
+
+    // Reallocate memory for pages
+    *(total_number_of_pages + x) = realloc(
+        *(total_number_of_pages + x),
+        (current_books + 1) * sizeof(int)
+    );
+
+    // Assign pages to new book
+    *(*(total_number_of_pages + x) + current_books) = y;
+        } else if (type_of_query == 2) {
+            int x, y;
+            scanf("%d %d", &x, &y);
+            printf("%d\n", *(*(total_number_of_pages + x) + y));
+        } else {
+            int x;
+            scanf("%d", &x);
+            printf("%d\n", *(total_number_of_books + x));
+        }
+    }
+
+    if (total_number_of_books) {
+        free(total_number_of_books);
+    }
+    
+    for (int i = 0; i < total_number_of_shelves; i++) {
+        if (*(total_number_of_pages + i)) {
+            free(*(total_number_of_pages + i));
+        }
+    }
+    
+    if (total_number_of_pages) {
+        free(total_number_of_pages);
+    }
+    
+    return 0;
+}
